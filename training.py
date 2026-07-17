51275c6880682839dcf28bcc7e8f1f86ca256fe2
Danil <shmulevdanik@gmail.com>
2026-07-17 22:49:14 +0700

    first day

---
+++ training.py
@@ -0,0 +1,9 @@
+potion = {
+    "name":                          "potion of strength",
+    "level":                         2, 
+    "buff":                         "gives x2 damage",
+    "multiplier":                    2,
+}
+for gam in potion:
+    print(f"Key: {potion[gam]}")
+    print(gam)
